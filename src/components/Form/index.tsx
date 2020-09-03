67e41fbb6091b054a3bacc621654446330cd210a
Tamires <tamires.manhaes@cubos.io>
2020-09-03 11:30:09 -0300

    form criado

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -5,17 +5,16 @@ const FormWrap: React.FC = () => {
   return(
     <div className="form-container">
       <div className="description">
-        <strong>Try it free 7 days</strong>
-        <span>then $20/mo. thereafter</span>
+        <strong>Try it free 7 days</strong>{' '}<span>then $20/mo. thereafter</span>
       </div>
       <form>
         <input type="text" placeholder="First Name" />
         <input type="text" placeholder="Last Name"/>
         <input type="text" placeholder="Email Address"/>
         <input type="text" placeholder="Password" />
-        <button>Subscribe</button>
+        <button>Claim your free trial</button>
 
-        <span>Claim your free trial By clicking the button, you are agreeing to our Terms and Services</span>
+        <span>By clicking the button, you are agreeing to our <a href="google.com" target="_blank">Terms and Services</a></span>
       </form>
     </div>
   );
